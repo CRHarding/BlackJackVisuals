dd3e9659d1a1b3ace19bff84bcf45d6246ff6da1
CRHarding <casey.r.harding@gmail.com>
2018-05-18 12:42:32 -0400

    User input on successful loading of background image

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ import processing.core.PImage;
 
 public class Main extends PApplet {
     private PImage img;
+    private String myText;
+
+    public Main() {
+        myText = "What is your name?";
+    }
+
     public static void main(String[] args) {
         PApplet.main("Main", args);
         System.out.println("Welcome to Black Jack!");
@@ -16,11 +22,27 @@ public class Main extends PApplet {
     public void setup() {
         img = loadImage("/Users/caseyrharding/IdeaProjects/BlackJackVisuals/src/CardGameImages/cardTable.png");
         img.resize(width, height);
-        Game game = new Game();
-        game.run();
+        textAlign (CENTER, CENTER);
+        textSize (30);
+        fill(0);
+//        Game game = new Game();
+//        game.run();
     }
 
     public void draw() {
         background(img);
+        text(myText, 0, 0, width, height);
+    }
+
+    public void keyPressed() {
+        if (keyCode == BACKSPACE) {
+            if (myText.length() > 0) {
+                myText = myText.substring(0, myText.length()-1);
+            }
+        } else if (keyCode == DELETE) {
+            myText = "";
+        } else if (keyCode != SHIFT && keyCode != CONTROL && keyCode != ALT) {
+            myText = myText + key;
+        }
     }
 }
